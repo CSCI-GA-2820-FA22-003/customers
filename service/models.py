30d99fd5cf95b9bcc476eb189ec9793fecd8a572
KaterinPerdom <katerinperdom9001@gmail.com>
2022-10-15 23:04:59 +0000

    creation of model customer with name, lastname,email, phone, adress,city, state, country and zipcode

--- service/models.py
+++ service/models.py
@@ -15,12 +15,10 @@ db = SQLAlchemy()
 class DataValidationError(Exception):
     """ Used for an data validation errors when deserializing """
 
-    pass
 
-
-class YourResourceModel(db.Model):
+class Customer(db.Model):
     """
-    Class that represents a YourResourceModel
+    Class that represents a customers
     """
 
     app = None
@@ -28,13 +26,21 @@ class YourResourceModel(db.Model):
     # Table Schema
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(63))
+    lastname = db.Column(db.String(63))
+    email = db.Column(db.String(120))
+    phone = db.Column(db.String(20))
+    address = db.Column(db.String(256))
+    city = db.Column(db.String(64))
+    state = db.Column(db.String(2))
+    country = db.Column(db.String(63))
+    zipcode = db.Column(db.String(16)) 
 
     def __repr__(self):
-        return "<YourResourceModel %r id=[%s]>" % (self.name, self.id)
+        return "<Customer %r id=[%s]>" % (self.name, self.id)
 
     def create(self):
         """
-        Creates a YourResourceModel to the database
+        Creates a Customer to the database
         """
         logger.info("Creating %s", self.name)
         self.id = None  # id must be none to generate next primary key
@@ -43,37 +49,54 @@ class YourResourceModel(db.Model):
 
     def update(self):
         """
-        Updates a YourResourceModel to the database
+        Updates a Customer to the database
         """
         logger.info("Saving %s", self.name)
         db.session.commit()
 
     def delete(self):
-        """ Removes a YourResourceModel from the data store """
+        """ Removes a Customer from the data store """
         logger.info("Deleting %s", self.name)
         db.session.delete(self)
         db.session.commit()
 
     def serialize(self):
-        """ Serializes a YourResourceModel into a dictionary """
-        return {"id": self.id, "name": self.name}
+        """ Serializes a Customer into a dictionary """
+        return {"id": self.id, 
+                "name": self.name,
+                "lastname": self.lastname,
+                "email": self.email,
+                "phone": self.phone,
+                "address": self.address,
+                "city": self.city,
+                "state": self.state,
+                "country": self.country,
+                "zipcode": self.zipcode
+                }
 
     def deserialize(self, data):
         """
-        Deserializes a YourResourceModel from a dictionary
+        Deserializes a Customer from a dictionary
 
         Args:
             data (dict): A dictionary containing the resource data
         """
         try:
             self.name = data["name"]
+            self.lastname = data["lastname"]
+            self.email = data["email"]
+            self.phone = data["phone"]
+            self.address = data["address"]
+            self.city = data["city"]
+            self.state = data["state"]
+            self.country = data["country"]
+            self.zipcode = data["zipcode"]
         except KeyError as error:
             raise DataValidationError(
-                "Invalid YourResourceModel: missing " + error.args[0]
-            )
+                "Invalid Customer: missing " + error.args[0])
         except TypeError as error:
             raise DataValidationError(
-                "Invalid YourResourceModel: body of request contained bad or no data - "
+                "Invalid Customer: body of request contained bad or no data - "
                 "Error message: " + error
             )
         return self
@@ -90,22 +113,22 @@ class YourResourceModel(db.Model):
 
     @classmethod
     def all(cls):
-        """ Returns all of the YourResourceModels in the database """
+        """ Returns all of the Customers in the database """
         logger.info("Processing all YourResourceModels")
         return cls.query.all()
 
     @classmethod
     def find(cls, by_id):
-        """ Finds a YourResourceModel by it's ID """
+        """ Finds a Customer by it's ID """
         logger.info("Processing lookup for id %s ...", by_id)
         return cls.query.get(by_id)
 
     @classmethod
     def find_by_name(cls, name):
-        """Returns all YourResourceModels with the given name
+        """Returns all Customers with the given name
 
         Args:
-            name (string): the name of the YourResourceModels you want to match
+            name (string): the name of the Customers you want to match
         """
         logger.info("Processing name query for %s ...", name)
         return cls.query.filter(cls.name == name)

74e3496068099a683b0fb4a63f4b4ba5e557da3a
vrangr1 <anavprasadanav@gmail.com>
2022-10-18 00:17:18 +0000

    update customer REST API completed; waiting on create customer branch to be merged for proper testing

--- service/routes.py
+++ service/routes.py
@@ -25,9 +25,47 @@ def index():
 
 
 ######################################################################
+# REST API TO UPDATE CUSTOMER'S PERSONAL DATA
+######################################################################
+@app.route("/customers/<int:customer_id>", methods=["PUT"])
+def update_customer(customer_id):
+    """
+    Update a customer's personal data.
+    This endpoint will update a customer's data based on the body that is posted
+    """
+    app.logger.info("Request to update the customer with id: %s", customer_id)
+    check_content_type("application/json")
+
+    # See if the account exists and abort if it doesn't
+    customer_account = Customer.find(customer_id)
+    if not customer_account:
+        abort(
+            status.HTTP_404_NOT_FOUND, f"Customer with id '{customer_id}' was not found."
+        )
+
+    # Update from the json in the body of the request
+    customer_account.deserialize(request.get_json())
+    customer_account.id = customer_id
+    customer_account.update()
+
+    return make_response(jsonify(customer_account.serialize()), status.HTTP_200_OK)
+
+
+######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
 
+
+def check_content_type(media_type):
+    """Checks that the media type is correct"""
+    content_type = request.headers.get("Content-Type")
+    if content_type and content_type == media_type:
+        return
+    app.logger.error("Invalid Content-Type: %s", content_type)
+    abort(
+        status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+        "Content-Type must be {}".format(media_type),
+    )
 
 def init_db():
     """ Initializes the SQLAlchemy app """
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -10,9 +10,11 @@ import logging
 from unittest import TestCase
 from unittest.mock import MagicMock, patch
 from service import app
-from service.models import db
-from service.common import status  # HTTP Status Codes
+from service.models import Customer, db
+from service.common import status
+from tests.factories import CustomerFactory  # HTTP Status Codes
 
+BASE_URL = "/customers"
 
 ######################################################################
 #  T E S T   C A S E S
@@ -46,3 +48,19 @@ class TestYourResourceServer(TestCase):
         """ It should call the home page """
         resp = self.app.get("/")
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
+    
+    def test_update_customer(self):
+        """It should Update a existing customer's data"""
+        # create an Account to update
+        test_account = CustomerFactory()
+        resp = self.app.post(BASE_URL, json=test_account.serialize())
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+
+        # update the pet
+        new_customer = resp.get_json()
+        new_customer["name"] = "Happy-Happy Joy-Joy"
+        new_customer_id = new_customer["id"]
+        resp = self.app.put(f"{BASE_URL}/{new_customer_id}", json=new_customer)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        updated_customer = resp.get_json()
+        self.assertEqual(updated_customer["name"], "Happy-Happy Joy-Joy")
